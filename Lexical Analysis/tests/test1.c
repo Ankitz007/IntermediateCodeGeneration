0bacdbf7fd2095520fb6e843dc2d52c4cb50432e
Ankit Kumar Singh <ankitkumarsingh11061999@gmail.com>
2021-01-06 23:03:01 +0530

    initial commit

---
+++ Lexical Analysis/tests/test1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    char ch;//Character Datatype
+
+    for (i=0;i<n;i++){
+        if(i<10){
+            int x;
+        }
+
+    }
+    /*
+    This File Contains Test cases about Datatypes,Keyword,Identifier,Nested For and while loop,
+    Conditional Statement,Single line Comment,MultiLine Comment etc.*/
+    
+}
---
+++ Lexical Analysis/tests/test4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 2;
+	printf("%d",a);
+	a++;
+	int b = 4;
+	int c = 3; 
+}
